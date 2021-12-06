ece4048d5aada0813a9ea7f75c750400a39b0b74
Gabriel <lino.dmarco@gmail.com>
2021-12-06 19:09:24 -0300

    add api integration tests

--- src/pages/Login/__tests__/Login.spec.tsx
+++ src/pages/Login/__tests__/Login.spec.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import {Login} from '../Login';
+import {Login, LoginInput} from '../Login';
 import {render, fireEvent, waitFor} from '@testing-library/react-native';
 import {ThemeProvider} from '../../../contexts';
 import renderer from 'react-test-renderer';
+import {User} from '../../../@types/user.interface';
+
+interface ApiPostResponse {
+  data: User;
+  headers: any;
+}
+
+let mockUrl: string;
+let mockData: LoginInput;
+jest.mock('../../../services/api', () => ({
+  post: jest.fn((url: string, data: LoginInput): Promise<ApiPostResponse> => {
+    mockUrl = url;
+    mockData = data;
+    return Promise.resolve({
+      data: {
+        id: 'test',
+        name: 'Tester User',
+        email: 'test@email.com',
+        birthdate: '1992',
+        gender: 'T',
+      },
+      headers: {
+        authorization: 'test',
+      },
+    });
+  }),
+}));
 
 const mockLogin = jest.fn((data: any) => data);
 jest.mock('../../../hooks', () => ({
@@ -42,6 +69,9 @@ describe('pages/Login', () => {
     await waitFor(() => fireEvent.press(loginButton));
 
     expect(mockLogin).toHaveBeenCalled();
+    expect(mockUrl).toBe('/auth/sign-in');
+    expect(mockData.email).toBe('test@email.com');
+    expect(mockData.password).toBe('123456');
   });
 
   it('should match snapshot', () => {
